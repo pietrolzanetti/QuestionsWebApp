708b23bfcb6209bf84b42058403e9634e8cd8852
pietrolzanetti <pietrolzanetti@gmail.com>
2022-11-16 02:11:17 -0300

    initial commit

---
+++ Data/Migrations/20221116043508_initialsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestionsWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AskedQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Question");
+        }
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace QuestionsWebApp.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string AskedQuestion { get; set; }
+        public string Answer { get; set; }
+
+        public Question()
+        {
+
+        }
+    }
+}
